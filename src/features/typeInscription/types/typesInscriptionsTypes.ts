32fdec7a1c746000f28549b781622dd3febfaf10
matheusDev <matheusfurtadogg@gmail.com>
2025-11-19 05:13:14 -0300

    Add 'specialType' field to TypeInscriptions and input types
    - Introduced 'specialType' boolean field in TypeInscriptions type for enhanced categorization.
    - Updated CreateTypeInscriptionInput and UpdateTypeInscriptionInput to include 'specialType' for consistency in data handling.

--- src/features/typeInscription/types/typesInscriptionsTypes.ts
+++ src/features/typeInscription/types/typesInscriptionsTypes.ts
@@ -2,6 +2,7 @@ export type TypeInscriptions = {
   id: string;
   description: string;
   value: number;
+  specialType: boolean;
   createdAt: Date;
   updatedAt: Date;
 };
@@ -10,9 +11,11 @@ export type CreateTypeInscriptionInput = {
   description: string;
   value: number;
   eventId: string;
+  specialType: boolean;
 };
 
 export type UpdateTypeInscriptionInput = {
   description?: string;
   value?: number;
+  specialType?: boolean;
 };
