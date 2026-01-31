96c7f9daa816dd370d53183e923620fe32d1c72d
cabeca <matheusfurtadogg@gmail.com>
2026-01-31 04:31:57 -0300

    feat: add PIX payment registration page component
    Add a new page component for PIX payment registration that displays instructions for users to complete their payment.

---
+++ src/app/(public)/guest/[eventId]/payment/pix/page.tsx
@@ -0,0 +1,10 @@
+export function RegisterPaymentViaPIX() {
+  return (
+    <div>
+      <p>
+        Para realizar o pagamento via PIX, por favor, acesse o link abaixo e
+        siga as instruções.
+      </p>
+    </div>
+  );
+}
