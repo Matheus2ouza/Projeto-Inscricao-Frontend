24810c1aa9ae22ad17dc43b8044cce43cd7b74d2
MatheusSouza <matheusfurtadogg@gmail.com>
2026-04-16 19:31:47 -0300

    refactor(payment): remove unused hooks, types, and schemas related to payment processing
    - Deleted hooks for listing pending payments, registering payments, and payment details.
    - Removed associated types and schemas that are no longer in use.
    - Cleaned up the payment-related codebase to improve maintainability and reduce complexity.

--- src/features/payment/types/adminDetailsPayment/actions/deletePaymentTypes.ts
+++
@@ -1,3 +0,0 @@
-export type DeletePaymentInput = {
-  paymentId: string;
-};
--- src/features/payment/types/adminDetailsPayment/actions/modifyReceiptPaymentTypes.ts
+++
@@ -1,9 +0,0 @@
-export type ModifyReceiptPaymentInput = {
-  paymentId: string;
-  image: string;
-};
-
-export type ModifyReceiptPaymentResponse = {
-  paymentId: string;
-  image: string;
-};
