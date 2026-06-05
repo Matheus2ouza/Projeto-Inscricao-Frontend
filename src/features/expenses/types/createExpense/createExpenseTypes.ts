5ff8e078d264a4d1b2495aa2780b256d34615de0
matheus <matheusfurtadogg@gmail.com>
2026-06-05 03:44:22 -0300

    feat: refactor expense creation API and types for better structure and clarity

---
+++ src/features/expenses/types/createExpense/createExpenseTypes.ts
@@ -0,0 +1,51 @@
+export type PaymentMethod = 'PIX' | 'CARTAO' | 'DINHEIRO';
+
+export enum CategoryExpense {
+  BRINDES = 'BRINDES',
+  COZINHA = 'COZINHA',
+  DECORACAO = 'DECORACAO',
+  DECORACAO_ESTACAO = 'DECORACAO_ESTACAO',
+  DECORACAO_COMPERADORES = 'DECORACAO_COMPERADORES',
+  MIDIA = 'MIDIA',
+  SOM = 'SOM',
+  MANUTENCAO = 'MANUTENCAO',
+  SEGURANCA = 'SEGURANCA',
+  OUTROS = 'OUTROS',
+}
+
+export type Expense = {
+  id: string;
+  eventId: string;
+  description: string;
+  value: number;
+  paymentMethod: PaymentMethod;
+  category: CategoryExpense;
+  image: string;
+  responsible: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type CreateExpenseRequest = {
+  eventId?: string;
+  description: string;
+  value: number;
+  paymentMethod: PaymentMethod;
+  category: CategoryExpense;
+  image: string;
+  responsible: string;
+  createAt?: string;
+};
+
+export type CreateExpenseResponse = {
+  id: string;
+  eventId: string;
+  description: string;
+  value: number;
+  paymentMethod: PaymentMethod;
+  category: CategoryExpense;
+  image: string;
+  responsible: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
